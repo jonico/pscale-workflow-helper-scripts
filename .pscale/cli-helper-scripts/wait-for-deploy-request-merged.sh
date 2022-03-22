9db8a4ade65b1e869af21fed6440b29825c7faf6
Johannes Nicolai <jonico@planetscale.com>
2022-03-22 12:39:14 +0100

    Better handling of edge cases

--- .pscale/cli-helper-scripts/wait-for-deploy-request-merged.sh
+++ .pscale/cli-helper-scripts/wait-for-deploy-request-merged.sh
@@ -37,12 +37,12 @@ function wait_for_deploy_request_merged {
                 echo  "Deploy request $number is not ready after $retries retries. Exiting..."
                 return 2
             fi
-            echo  "Deploy-request $number is not merged yet. Current status:"
+            echo  "Deploy-request $number is not deployed yet. Current status:"
             echo "show vitess_migrations\G" | pscale shell "$db" main --org "$org"
             echo "Retrying in $wait seconds..."
             sleep $wait
-        elif [ "$output" = "\"complete\"" ]; then
-            echo  "Deploy-request $number has been merged successfully."
+        elif [ "$output" = "\"complete\"" ] || [ "$output" = "\"complete_pending_revert\"" ]; then
+            echo  "Deploy-request $number has been deployed successfully."
             return 0
         else
             echo  "Deploy-request $number with unknown status: $output"
